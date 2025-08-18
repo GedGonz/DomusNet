21398d804697ce382f16a3f74b32c31471a2f00e
Gerald Gonzalez <gedgonz7@gmail.com>
2025-08-18 16:22:34 -0600

    init project

---
+++ src/main/java/com/domus/net/DomusNetApplication.java
@@ -0,0 +1,13 @@
+package com.domus.net;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomusNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomusNetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/domus/net/DomusNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domus.net;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomusNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
